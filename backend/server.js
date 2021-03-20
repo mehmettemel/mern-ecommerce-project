b1ec3cdc364ecb46df96351cefb6dc6b8040a380
mehmet temel <temel1072@hotmail.com>
2021-03-20 12:08:57 +0300

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -29,12 +29,13 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
+
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
-    res.send('API IS RUNNING')
+    res.send('API is running....')
   })
 }
 
